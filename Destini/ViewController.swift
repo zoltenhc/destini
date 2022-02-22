254dba27d2e4726405800ed59b2221b5b035e76a
zoltenhc <zoltenhc@gmail.com>
2022-02-22 22:03:06 +0100

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Zoltán Gál on 2022. 02. 22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
